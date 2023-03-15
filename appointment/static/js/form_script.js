ace2ef48b892358ec25552e4d8d42139436d92ad
old_qqw <theavg@yandex.ru>
2023-03-15 21:44:10 +0400

    fix: processing form fields with javascript

--- appointment/static/js/form_script.js
+++ appointment/static/js/form_script.js
@@ -1,13 +1,17 @@
-let form = document.querySelector('form')
+let form = document.getElementsByClassName('post-form');
 let theme = document.getElementById('id_topic_title');
 let supervisor = document.getElementById('id_supervisor_name');
 
 form.method = "POST";
 
-supervisor.setAttribute('disabled', '');
+if (document.body.contains(supervisor)) {
+    supervisor.setAttribute('disabled', '');
+}
 
-theme.addEventListener('change', function () {
-    if (theme.value != '---------') {
-        supervisor.removeAttribute('disabled');
-    }
-})
+if (document.body.contains(theme)) {
+    theme.addEventListener('change', function () {
+        if (theme.value != '---------') {
+            supervisor.removeAttribute('disabled');
+        }
+    })
+}
