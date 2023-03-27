373bfafdaf8ecea2b9519c57d7c6e819ba5c0579
old_qqw <theavg@yandex.ru>
2023-03-28 00:34:28 +0400

    feat: secret key & debug from env

--- RaspredDiplom/settings.py
+++ RaspredDiplom/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q)b+ur3675-1_jf7bm98mf)63z0s_$cynbgmtbqsv6q(rt#pb_'
+# environ settings
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
+environ.Env.read_env()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG_DJANGO')
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
 
 match DEBUG:
     case True:
